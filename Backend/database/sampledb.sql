08c63c07a0914ade9782843aa5981693f642e872
obedsp22 <obedp375@gmail.com>
2024-03-07 18:26:29 -0500

    add sampledb

--- Backend/database/sampledb.sql
+++ Backend/database/sampledb.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `computer_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `computer_store`;
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: computer_store
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,10 +26,10 @@ DROP TABLE IF EXISTS `categories`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
   `CategoryId` int NOT NULL AUTO_INCREMENT,
-  `Cat_Name` varchar(200) NOT NULL,
+  `Name` varchar(200) NOT NULL,
   `Description` varchar(200) NOT NULL,
   PRIMARY KEY (`CategoryId`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Hard Drives','Expand your computers storage.'),(5,'Laptops','Variety of laptops to choose from.'),(6,'Monitors','Choose a size that best fits you.');
+INSERT INTO `categories` VALUES (3,'Hardware','Motherboard Equipment'),(4,'Chassis','Plastic & Metal equipment');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ CREATE TABLE `customers` (
   `Country` varchar(200) NOT NULL,
   `Phone` varchar(45) NOT NULL,
   PRIMARY KEY (`CustomerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES (2,'Mike','Brown','75 Mid Cape','Cape','33914','USA','123-456-7890'),(4,'Jon','Anik','123 West St','Naples','34115','USA','239-999-9999'),(9,'Luna','Diaz','123 Name St','Miami','33166','USA','987-654-3121'),(11,'John','Skylark','123 Place Dr','Detroit','48127','USA','263-999-9988');
+INSERT INTO `customers` VALUES (2,'Mike','Brown','75 Mid Cape','Cape','33914','USA','123-456-7890'),(4,'Jon','Anik','123 West St','Naples','34102','USA','239-999-9999'),(5,'Sean','Doe','46578 Street Dr','BigCity','12345','USA','456-456-7898');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +86,7 @@ CREATE TABLE `employees` (
   `DOB` date NOT NULL,
   `Email` varchar(200) NOT NULL,
   PRIMARY KEY (`EmployeeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +95,7 @@ CREATE TABLE `employees` (
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (2,'Peter','Pan','1991-04-25','peter@pan'),(8,'Christina','Medford','1991-08-31','christina@med'),(10,'Terrance','Crawford','1987-09-28','terrance@crawford');
+INSERT INTO `employees` VALUES (1,'Jay','Parker','1995-01-26','jason.p@hotmail.com'),(2,'Amanda','Nunes','1995-05-15','amanda.n@yahoo.com');
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,7 +119,7 @@ CREATE TABLE `orders` (
   CONSTRAINT `ForeignKey_customer` FOREIGN KEY (`Customer`) REFERENCES `customers` (`CustomerId`) ON DELETE CASCADE,
   CONSTRAINT `ForeignKey_employee` FOREIGN KEY (`Employee`) REFERENCES `employees` (`EmployeeId`) ON DELETE CASCADE,
   CONSTRAINT `ForeignKey_shipper` FOREIGN KEY (`Shipper`) REFERENCES `shippers` (`ShipperId`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +128,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,'2023-08-16',5,9,8),(3,'2023-06-30',6,4,2);
+INSERT INTO `orders` VALUES (1,'2024-03-15',1,5,1);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,7 +141,7 @@ DROP TABLE IF EXISTS `products`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
   `ProductId` int NOT NULL AUTO_INCREMENT,
-  `Prod_Name` varchar(200) NOT NULL,
+  `Name` varchar(200) NOT NULL,
   `Unit` int NOT NULL,
   `Price` decimal(10,2) NOT NULL,
   `Category` int NOT NULL,
@@ -151,7 +151,7 @@ CREATE TABLE `products` (
   KEY `FK_for_Category_idx` (`Category`),
   CONSTRAINT `FK_for_Category` FOREIGN KEY (`Category`) REFERENCES `categories` (`CategoryId`) ON DELETE CASCADE,
   CONSTRAINT `FK_for_Supplier` FOREIGN KEY (`Supplier`) REFERENCES `suppliers` (`SupplierId`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,7 +160,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Dell Laptop',1,850.00,5,1),(6,'MacBook',1,1500.00,5,1),(8,'32 GB Hard Drive',2,94.99,1,3),(9,'36\' Hp monitor',1,54.99,1,3);
+INSERT INTO `products` VALUES (1,'Computer Hardware Inc.',500,26.98,3,1);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,10 +173,10 @@ DROP TABLE IF EXISTS `shippers`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shippers` (
   `ShipperId` int NOT NULL AUTO_INCREMENT,
-  `Ship_Name` varchar(200) NOT NULL,
+  `Name` varchar(200) NOT NULL,
   `Phone` varchar(45) NOT NULL,
   PRIMARY KEY (`ShipperId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,7 +185,7 @@ CREATE TABLE `shippers` (
 
 LOCK TABLES `shippers` WRITE;
 /*!40000 ALTER TABLE `shippers` DISABLE KEYS */;
-INSERT INTO `shippers` VALUES (4,'Shipmates','123-456-8888'),(5,'QuickShippers','555-888-9899'),(6,'Maersk','222-655-6565');
+INSERT INTO `shippers` VALUES (1,'Movers, Co.','989-888-5454'),(2,'Quick Pickups, LLC','245-656-6363');
 /*!40000 ALTER TABLE `shippers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -198,14 +198,14 @@ DROP TABLE IF EXISTS `suppliers`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `suppliers` (
   `SupplierId` int NOT NULL AUTO_INCREMENT,
-  `Sup_Name` varchar(200) NOT NULL,
+  `Name` varchar(200) NOT NULL,
   `Address` varchar(200) NOT NULL,
   `City` varchar(200) NOT NULL,
   `Zip` varchar(45) NOT NULL,
   `Country` varchar(200) NOT NULL,
   `Phone` varchar(45) NOT NULL,
   PRIMARY KEY (`SupplierId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -214,7 +214,7 @@ CREATE TABLE `suppliers` (
 
 LOCK TABLES `suppliers` WRITE;
 /*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
-INSERT INTO `suppliers` VALUES (1,'Computers.Inc','123 Company St Sw','Naples','34102','USA','444-444-8989'),(3,'Supplier.co','987 Supplier St E','Ft. Myers','33908','USA','888-454-8989'),(4,'LightMonitors.inc','123 Thorton Dr.','Denver','80123','USA','555-468-2929');
+INSERT INTO `suppliers` VALUES (1,'Computer Hardware Inc.','123 Street Pl','LA','78965','USA','789-456-7899'),(2,'Tech City','789','Berlin','44444','Germany','888-988-8989');
 /*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -227,4 +227,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-21 13:36:41
+-- Dump completed on 2024-03-07  7:34:02
